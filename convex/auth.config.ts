a1bb562edf5ef027fd6b84cdb4212c18fbc3a69f
instasif <asifh9277@gmail.com>
2024-11-26 22:25:32 +0600

    added convex & clerk for backend

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    providers: [
+      {
+        domain: "https://healthy-stinkbug-4.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
