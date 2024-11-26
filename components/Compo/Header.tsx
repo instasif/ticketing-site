88d58c03b79a0a9d9ee05f7f2250e3a6a4b7392f
instasif <asifh9277@gmail.com>
2024-11-26 23:44:32 +0600

    added responsive header and signup signout by clerk

---
+++ components/Compo/Header.tsx
@@ -0,0 +1,39 @@
+// Header.jsx or Header.tsx
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/images/logo.png";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import SearchBar from "@/components/Compo/SearchBar"
+
+export default function Header() {
+  return (
+    <div className="border-b">
+      <div className="flex flex-col lg:flex-row items-center gap-4 p-4">
+        <div className="flex items-center justify-between w-full lg:w-auto">
+          <Link href={"/"}>
+            <Image src={logo} alt="logo" width={100} height={100} className="w-24 lg:w-28" />
+          </Link>
+
+          <div className="lg:hidden">
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+            <SignedOut>
+              <SignInButton mode="modal">
+                <button className="bg-gray-100 px-3 py-1.5 text-sm rounded-lg hover:bg-gray-200 transition border border-gray-300">
+                  Sign In
+                </button>
+              </SignInButton>
+            </SignedOut>
+          </div>
+        </div>
+
+        <div className="w-full lg:max-w-2xl">
+            <SearchBar />
+        </div>
+      </div>
+    </div>
+  );
+}
