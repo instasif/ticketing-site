47be51c9f7e6a0d868fa12e03b30b36acd8f2324
instasif <asifh9277@gmail.com>
2024-12-09 01:49:51 +0600

    added stripe and patch stripeConnectId of user on db

--- actions/createStripeConnectCustomer.ts
+++ actions/createStripeConnectCustomer.ts
@@ -1,9 +1,48 @@
-"user server"
+("user server");
 
-import { ConvexHttpClient } from "convex/browser"
+import { api } from "@/convex/_generated/api";
+import { stripe } from "@/lib/stripe";
+import { auth } from "@clerk/nextjs/server";
+import { ConvexHttpClient } from "convex/browser";
 
 if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
-    throw new Error("NEXT_PUBLIC_CONVEX_URL is not set")
+  throw new Error("NEXT_PUBLIC_CONVEX_URL is not set");
 }
 
 const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL);
+
+export async function createStripeConnectCustomer() {
+  const { userId } = await auth();
+  if (!userId) {
+    throw new Error("Not authenticated");
+  }
+
+  // chack if user already has a connect account
+  const existingStripeConnectId = await convex.query(
+    api.users.getUsersStripeConnectId,
+    {
+      userId,
+    }
+  );
+
+  if (existingStripeConnectId) {
+    return { account: existingStripeConnectId };
+  }
+
+  // create new connect account
+  const account = await stripe.accounts.create({
+    type: "express",
+    capabilities: {
+      card_payments: { requested: true },
+      transfers: { requested: true },
+    },
+  });
+
+  //update user with stripe connect id
+  await convex.mutation(api.users.updateOrCreateUserStripeConnectId, {
+    userId,
+    stripeConnetId: account.id,
+  });
+
+  return { account: account.id };
+}
